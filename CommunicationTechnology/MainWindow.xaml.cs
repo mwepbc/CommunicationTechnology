1c59f3e00e92bee9c1c15400f87fc89d18925cc4
kalim <kalim@DESKTOP-A48EB3K>
2025-07-01 19:52:36 +0500

    Добавьте файлы проекта.

---
+++ CommunicationTechnology/MainWindow.xaml.cs
@@ -0,0 +1,66 @@
+﻿using CommunicationTechnology.Pages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CommunicationTechnology
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            //метод для инициализации всех компонентов в странице
+            InitializeComponent();
+
+            //Чтобы при запуске приложения загрузить в MainFrame страницу личного кабинета
+            //используем метод Navigate
+            MainFrame.Navigate(new PersonalPage());
+
+            //Проблема - cs файл может не увидеть вашу страницы в папке Pages
+            //для решения достаточно в начале странице добавить ещё один using
+            //с указанием вашей папки в проекте
+
+            //достаточно просто правильно прописать название страницы и рефакторинг
+            //сам пропишет нужные строчки
+        }
+
+        private void BackButton_Click(object sender, RoutedEventArgs e)
+        {
+            //при нажатии на кнопку 'назад' будет производится проверка
+            //а можно ли вообще в фрейме вернуться на предыдущую страницу
+            //если да, то фрейм перебрасывает нас назад
+            if (MainFrame.CanGoBack)
+            {
+                MainFrame.GoBack();
+            }
+        }
+
+        private void MainFrame_ContentRendered(object sender, EventArgs e)
+        {
+            //соответственно, если в фрейме нет предыдущих страниц, то кнопка
+            //назад попросту не отображается
+            if (!MainFrame.CanGoBack)
+            {
+                BackButton.Visibility = Visibility.Collapsed;
+            }
+            else
+            {
+                BackButton.Visibility = Visibility.Visible;
+            }
+        }
+    }
+}
---
+++ CommunicationTechnology/Pages/PersonalPage.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CommunicationTechnology.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для PersonalPage.xaml
+    /// </summary>
+    public partial class PersonalPage : Page
+    {
+        public PersonalPage()
+        {
+            InitializeComponent();
+        }
+
+        private void TariffButton_Click(object sender, RoutedEventArgs e)
+        {
+            //this - указывает на текущий элемент, в котором происходит событие
+            //NavigationService, собственно, вызывает сервис навигации по страницам
+            //метод Navigate перебрасывает на указанную страницу
+            this.NavigationService.Navigate(new TariffPage());
+        }
+
+        private void CostButton_Click(object sender, RoutedEventArgs e)
+        {
+            //класс MessageBox отвечает за выполнение окна сообщения
+            //метод Show отображает окно с сообщением, указываемым в скобках
+            MessageBox.Show("Страница в разработке");
+        }
+
+        private void ServicesButton_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Страница в разработке");
+        }
+
+        private void SupportButton_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Страница в разработке");
+        }
+    }
+}
